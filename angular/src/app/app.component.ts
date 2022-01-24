d952aba959a79805ddfea3bcb89ba15a615998f2
Rodrigo Lago <rodrigo.lago@situm.com>
2022-01-24 14:48:29 +0100

    Added some buttons to test features.

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,5 +1,20 @@
-import { Component } from '@angular/core';
-import { SitumWayfinding } from 'situm-capacitor-plugin-wayfinding';
+import { 
+  Component,
+  NgZone
+ } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { 
+  SitumWayfinding,
+  OnPoiSelectedResult,
+  OnPoiDeselectedResult,
+  OnFloorChangeResult } 
+  from 'situm-capacitor-plugin-wayfinding';
+
+const MY_BUILDING_ID = "---";
+const MY_POI_ID = "---";
+const MY_FLOOR_ID = "---";
+const MY_NAV_POINT_LAT = 0.0;
+const MY_NAV_POINT_LNG = 0.0;
 
 @Component({
   selector: 'app-root',
@@ -9,6 +24,14 @@ import { SitumWayfinding } from 'situm-capacitor-plugin-wayfinding';
 export class AppComponent {
   title = 'angular';
   maps: any = {};
+  captureTouchEvents = true;
+  showOuterHTML = false;
+  autostartMode = "centerPoi";
+
+  constructor(
+    private ngZone: NgZone,
+    private toastController: ToastController
+    ) {}
 
   async startMap(targetId: string) {
     const element: HTMLElement | null = document.getElementById(targetId);
@@ -30,18 +53,44 @@ export class AppComponent {
     try {
       // LibrarySettings:
       const librarySettings = {
-        user: "YOUR_SITUM_USER",
-        apiKey: "YOUR_SITUM_APIKEY",
+        user: "---",
+        apiKey: "---",
         iosGoogleMapsApiKey: "YOUR_IOS_GOOGLE_MAPS_APIKEY",
-        buildingId : "YOUR_BUILDING_ID",
+        buildingId : MY_BUILDING_ID,
         dashboardUrl: "https://dashboard.situm.com",
         hasSearchView: true,
         searchViewPlaceholder: "Capacitor WYF",
-        useDashboardTheme: false,
+        useDashboardTheme: true,
       };
       console.log(`ATAG: will call now SitumWayfinding#load(${JSON.stringify(librarySettings)})`)
       const wyfResponse = await SitumWayfinding.load(element, librarySettings);
       console.log(`ATAG: call to load finished with result: ${JSON.stringify(wyfResponse)}`);
+      // Map interactions:
+      // 1) POIs callback:
+      SitumWayfinding.onPoiSelected((result: OnPoiSelectedResult) => {
+        console.log("On poi selected callback!");
+        console.log(JSON.stringify(result));
+        this.presentToast(`Selected ${result.poiName} at ${result.buildingName}`);
+      });
+      SitumWayfinding.onPoiDeselected((result: OnPoiDeselectedResult) => {
+        console.log("On poi selected callback!");
+        console.log(JSON.stringify(result));
+        this.presentToast(`Deselected poi at ${result.buildingName}`);
+      });
+      // 2) Floor callback:
+      SitumWayfinding.onFloorChange((result: OnFloorChangeResult) => {
+        console.log("On floor change callback!");
+        console.log(JSON.stringify(result));
+        this.presentToast(`Floor changed from ${result.fromFloorName} to ${result.toFloorName}`);
+      });
+      // 3) Center POI:
+      if (this.autostartMode == "centerPoi"){
+        this.centerPoi();
+      } else if (this.autostartMode == "navToPoi") {
+        this.navToPoi();
+      } else if (this.autostartMode == "navToLocation") {
+        this.navToLocation();
+      }
 
     } catch (e) {
       alert('ATAG: Some kind of mysterious error just happened!!!');
@@ -63,6 +112,67 @@ export class AppComponent {
   async grow() {
     const element = document.getElementById("mapOverlay");
     element?.classList.add("big");
+  }
+
+  async presentToast(text: string) {
+    console.log('Call to presentToast()');
+    /*const toast = await this.toastController.create({
+      message: text,
+      duration: 1500
+    });
+    toast.present();*/
+    //alert(text);
+  }
+
+  async switchCaptureEvents() {
+    this.captureTouchEvents = !this.captureTouchEvents;
+    SitumWayfinding.setCaptureTouchEvents({
+      captureEvents: this.captureTouchEvents
+    });
+  }
+
+  async toggleOuterHTML() {
+    this.showOuterHTML = !this.showOuterHTML;
+    const oldClass = this.showOuterHTML ? "out" : "in";
+    const newClass = this.showOuterHTML ? "in" : "out";
+    const e = document.getElementById("outerHTML");
+    e?.classList.remove(oldClass);
+    e?.classList.add(newClass);
+  }
+
+  async onAutostartModeChanged(target: any) {
+    if (target && target.value) this.autostartMode = target.value;
+  }
+
+  async centerPoi() {
+    await SitumWayfinding.selectPoi({ 
+      id: MY_POI_ID,
+      buildingId: MY_BUILDING_ID
+     });
+  }
+
+  async centerBuilding() {
+    await SitumWayfinding.selectBuilding({ id: MY_BUILDING_ID });
+  }
+
+  async navToPoi() {
+    await SitumWayfinding.navigateToPoi({ 
+      id: MY_POI_ID,
+      buildingId: MY_BUILDING_ID
+     });
+  }
+
+  async navToLocation() {
+    await SitumWayfinding.navigateToLocation({ 
+      buildingId: MY_BUILDING_ID,
+      floorId: MY_FLOOR_ID,
+      latitude: MY_NAV_POINT_LAT,
+      longitude: MY_NAV_POINT_LNG 
+     });
+  }
+
+  async stopPositioning() {
+    await SitumWayfinding.stopPositioning();
   }
 }
 
